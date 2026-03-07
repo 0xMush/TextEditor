93226f0fdaa4f169711dcb9d8a7f2f9130c1e25d
0xMush <mushaibali7t1@gmail.com>
2026-03-07 17:48:35 -0500

    refreshscreen

--- supereditorbyme.c
+++ supereditorbyme.c
@@ -3,39 +3,80 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <errno.h>
+
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 struct termios orig_termios;
 
+/*** TERMINAL ***/
+
+void die(const char *s){
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == 1)
+    die("tcsetattr");
+
 }
 
 
 void EnableRawMode(){
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == 1) die("tcgetattr");
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_iflag &= ~(IXON | ICRNL);
+    raw.c_iflag &= ~(IXON | ICRNL | INPCK | ISTRIP | BRKINT);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == 1) die("tcsetattr");
 }
+
+char editorReadKey(){
+    int nread;
+    char c;
+    while((nread = read(STDIN_FILENO, &c, 1)) != 1){
+        if(nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+/*** output ***/
+void editorRefreshScreen(){
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*** input ***/
+
+
+void editorProcessKeypress(){
+    char c = editorReadKey();
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        exit(0);
+        break;
+    
+    }
+}
+
+/*** init ***/
 
 
 int main() {
-
   EnableRawMode();
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
-    if (iscntrl(c)){
-        printf("%d\n", c);
-    }
-    else{
-        printf("%d ('%c')\n", c, c);
-    }
-
-  }
-  
-  return 0;
+  while (1) {
+        editorProcessKeypress();
+        editorProcessKeypress();
+   }
+   return 0;
 
 }
